67864ff6adfb26d653f27f919cba72c2ab6de0b1
marek <marko.ognjenovic@prezly.com>
2020-12-06 17:52:32 +0100

    Define non primitive properties of OpenAPI model

--- src/Models/OpenAPI.php
+++ src/Models/OpenAPI.php
@@ -6,5 +6,65 @@ namespace Restz\OpenAPI\Models;
 
 class OpenAPI
 {
-    protected $openapi;
+    /**
+     * REQUIRED. This string MUST be the semantic version number of the OpenAPI Specification
+     * version that the OpenAPI document uses. The openapi field SHOULD be used by
+     * tooling specifications and clients to interpret the OpenAPI document.
+     * This is not related to the API info.version string.
+     *
+     * @var string
+     */
+    protected string $openapi;
+    /**
+     * REQUIRED. Provides metadata about the API. The metadata MAY be used by tooling as required.
+     *
+     * @var Info
+     */
+    protected Info $info;
+    /**
+     * An array of Server Objects, which provide connectivity information to a target server.
+     * If the servers property is not provided, or is an empty array, the default value would
+     * be a Server Object with a url value of /.
+     *
+     * @var Server[]
+     */
+    protected array $servers;
+    /**
+     * REQUIRED. The available paths and operations for the API.
+     *
+     * @var Paths
+     */
+    protected Paths $paths;
+    /**
+     * An element to hold various schemas for the specification.
+     *
+     * @var Components
+     */
+    protected Components $components;
+    /**
+     * A declaration of which security mechanisms can be used across the API.
+     * The list of values includes alternative security requirement objects that can be used.
+     * Only one of the security requirement objects need to be satisfied to authorize a request.
+     * Individual operations can override this definition. To make security optional,
+     * an empty security requirement ({}) can be included in the array.
+     *
+     * @var SecurityRequirement[]
+     */
+    protected array $security;
+    /**
+     * A list of tags used by the specification with additional metadata.
+     * The order of the tags can be used to reflect on their order by the parsing tools.
+     * Not all tags that are used by the Operation Object must be declared.
+     * The tags that are not declared MAY be organized randomly or based on the tools' logic.
+     * Each tag name in the list MUST be unique.
+     *
+     * @var array
+     */
+    protected array $tags;
+    /**
+     * Additional external documentation.
+     *
+     * @var ExternalDocumentation
+     */
+    protected ExternalDocumentation $external_doc;
 }
